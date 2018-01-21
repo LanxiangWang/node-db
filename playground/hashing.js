75fcbc57156dd5b5c12ef32420f377722ddf9b85
Lanxiang Wang <lanxiang.wang13@gmail.com>
2018-01-21 18:46:42 -0500

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -25,17 +25,46 @@
 //   console.log('Dangerous');
 // }
 
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-// jwt.sign
-// jwt.verify
+// // jwt.sign
+// // jwt.verify
 
-var data = {
-  id: 10
-};
+// var data = {
+//   id: 10
+// };
 
-var token = jwt.sign(data, '123');
-console.log(token);
+// var token = jwt.sign(data, '123');
+// console.log(token);
 
-var decoded = jwt.verify(token, '123');
-console.log(decoded);
+// var decoded = jwt.verify(token, '123');
+// console.log(decoded);
+
+const bcrypt = require('bcryptjs');
+
+var password = '123abc';
+
+
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
+
+var hashedPassword = '$2a$10$DWF3BY3kHurpeW9kGWQ12u2Sd2eOCDgwIBOtUzwJ.TKnKNRMt5Gtm';
+
+bcrypt.compare('123abc', hashedPassword, (err, res) => {
+  console.log(res);
+})
+
+
+
+
+
+
+
+
+
+
+
+
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -103,7 +104,25 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.token': token,
     'tokens.access': 'auth'
   })
-}
+};
+
+UserSchema.pre('save', function (next) {
+  var user = this;
+  console.log('pre save', user);
+
+  // isModified always return true for new instance that hasn't been saved
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } 
+  else {
+    next();
+  }
+});
 
 var Users = mongoose.model('Users', UserSchema);
 
